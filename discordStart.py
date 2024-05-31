9940d00098275b03421bb764b0a20ec69e4a4fe6
Vincent Raia <vincentraia1@gmail.com>
2024-05-30 22:28:06 -0400

    build out first few features

--- discordStart.py
+++ discordStart.py
@@ -1,11 +1,19 @@
 import discord
 import tokenSecret as ts
+import discordPhrases
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
     
     async def on_message(self, message):
+        if message.author == client.user:
+            return
+        
+        if message.content.startswith('$kramnik'):
+            await message.channel.send(discordPhrases.kramnik1)
+            await message.channel.send(discordPhrases.kramnik2)    
+
         print(f'Message from {message.author}:{message.content}')
 
 intents = discord.Intents.default()
